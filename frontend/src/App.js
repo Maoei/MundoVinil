b75e3d35040c6719379d8a7c4e70525d6a3a9afe
Marcelle Viana <marcellevianadasneves@gmail.com>
2023-03-21 20:52:00 -0300

    List Produtos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
       <header>
         <a href="/">Mundo Vinil</a>
       </header>
-      <main>listar produtos</main>
+      <main>
+        <h1>Nossos Produtos</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.url}>
+              <a href={`/product/${product.url}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.url}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>R${product.price}</strong>
+                </p>
+                <button>Adicionar ao Carrinho</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+  products: [
+    {
+      name: 'O Descobridor dos Sete Mares',
+      url: 'o-descobridor-dos-sete-mares',
+      category: 'Nacional',
+      image: '/images/pg1.jpg', // 679px × 829px
+      price: 100,
+      countInStock: 5,
+      artist: 'Tim Maia',
+      description:
+        'O Descobridor dos Sete Mares é um álbum de estúdio do cantor e compositor brasileiro Tim Maia, lançado em 1983 pela gravadora PolyGram.',
+    },
+    {
+      name: 'Metallica',
+      url: 'metallica',
+      category: 'Heavy Metal',
+      image: '/images/p2.jpg', // 679px × 829px
+      price: 80,
+      countInStock: 10,
+      artist: 'Metallica',
+      description:
+        'Metallica, também conhecido como The Black Album, é o quinto álbum de estúdio da banda norte-americana de heavy metal Metallica, lançado a 12 de agosto de 1991.',
+    },
+    {
+      name: 'Live After Death',
+      url: 'live-after-death',
+      category: 'Heavy Metal',
+      image: '/images/p3.jpg',
+      price: 90,
+      countInStock: 7,
+      artist: 'Iron Maiden',
+      description:
+        'Live After Death é um álbum ao vivo da banda inglesa de heavy metal Iron Maiden.',
+    },
+    {
+      name: 'Rita Lee',
+      url: 'rita-lee',
+      category: 'Nacional',
+      image: '/images/p4.png',
+      price: 105,
+      countInStock: 3,
+      artist: 'Rita Lee',
+      description:
+        'Rita Lee, também conhecido como Mania de Você é o sétimo álbum de estúdio lançado pela cantora de rock brasileira Rita Lee, em 1979.',
+    },
+  ],
+};
+export default data;
