d3dd6d74e51ac931592b22e4c4eedaf286ec7b50
Marcelle Viana <marcellevianadasneves@gmail.com>
2023-03-21 01:13:02 -0300

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Mundo Vinil</a>
+      </header>
+      <main>listar produtos</main>
+    </div>
+  );
+}
+
+export default App;
